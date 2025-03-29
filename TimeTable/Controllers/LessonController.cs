c5ada9bba61bad4030f624cd2eb9d0bb422636ad
parshutinnd <niki.saransk2016@gmail.com>
2025-03-29 21:16:34 +0300

    first commit

---
+++ TimeTable/Controllers/LessonController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimeTable.Models;
+using TimeTable.Data;
+using TimeTable.Models.Entity;
+using System.Data;
+
+namespace TimeTable.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LessonController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public LessonController(ApiContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public JsonResult GetAll()
+        {
+            var result = _context.Lessons.ToList();
+
+            if (result == null)
+                return new JsonResult(NotFound());
+
+            return new JsonResult(Ok(result));
+        }
+
+        [HttpGet]
+        public JsonResult GetById(int id)
+        {
+            var result = _context.Lessons.Find(id);
+
+            if (result == null)
+                return new JsonResult(NotFound());
+
+            return new JsonResult(Ok(result));
+        }
+
+
+        [HttpPost]
+        public JsonResult Create(Lesson lesson)
+        { 
+            _context.Lessons.Add(lesson);
+
+            _context.SaveChanges();
+            return new JsonResult(Ok());
+        }
+         
+    }
+}
---
+++ TimeTable/Data/ApiContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeTable.Models;
+using TimeTable.Models.Entity;
+
+namespace TimeTable.Data
+{
+    public class ApiContext : DbContext 
+    {
+        public DbSet<Lesson> Lessons { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+    }
+}
