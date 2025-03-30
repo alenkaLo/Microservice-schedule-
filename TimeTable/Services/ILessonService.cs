1ca91f3d33fd14ef91f400e792d96bd25a584368
parshutinnd <niki.saransk2016@gmail.com>
2025-03-30 12:23:51 +0300

    добавил слои для бизнес логики и взаимодействия с бд

---
+++ TimeTable/Services/ILessonService.cs
@@ -0,0 +1,12 @@
+﻿using TimeTable.Models.Entity;
+
+namespace TimeTable.Services
+{
+    public interface ILessonService
+    {
+        Task<List<Lesson>> GetAllLessons();
+        Task<Lesson> GetLessonById(Guid id);
+
+        Task<Guid> Add(Lesson lesson);
+    }
+}
