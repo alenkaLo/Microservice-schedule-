0b32d8f6586547a01335e37657e45af3320d8642
parshutinnd <niki.saransk2016@gmail.com>
2025-03-31 20:02:39 +0300

    исправлено MarkID -> MarkId

--- TimeTable/Models/Entity/Lesson.cs
+++ TimeTable/Models/Entity/Lesson.cs
@@ -7,7 +7,7 @@ namespace TimeTable.Models.Entity
         public Guid Id { get; set; }
         public Guid SubjectId { get; set; }
         public Guid UserId { get; set; }
-        public Guid MarkID { get; set; }
+        public Guid MarkId { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public Subgect? Subgect { get; set; }
