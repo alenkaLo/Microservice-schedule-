0a9fcf5656589a00cf35b8a572059cafba62b17c
parshutinnd <niki.saransk2016@gmail.com>
2025-03-31 19:53:23 +0300

    вернул markid

--- TimeTable/Models/Entity/Lesson.cs
+++ TimeTable/Models/Entity/Lesson.cs
@@ -7,7 +7,9 @@ namespace TimeTable.Models.Entity
         public Guid Id { get; set; }
         public Guid SubjectId { get; set; }
         public Guid UserId { get; set; }
+        public Guid MarkID { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
+        public Subgect? Subgect { get; set; }
     }
 }
--- TimeTable/Models/Entity/Subgect.cs
+++ TimeTable/Models/Entity/Subgect.cs
@@ -3,7 +3,6 @@
     public class Subgect
     {
         public Guid Id { get; set; }
-
         public string Name { get; set; }
     }
 }
