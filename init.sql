12834b1b0b43cd761f52aca297df92e7417d0187
alenkaLo <92675448+alenkaLo@users.noreply.github.com>
2025-04-08 19:28:29 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,9 +1,9 @@
 create table lesson (
 	lesson_id	uuid		primary key
 	, subject_id 	uuid		not null	
-	, start_time	timestamp	not null
-	, end_time	timestamp	not null
-	, date_lesson	timestamp	not null
+	, start_time	time     	not null
+	, end_time	time    	not null
+	, date_lesson	date    	not null
 	, name_class	varchar(8)	not null
 	, teacher_id	uuid		not null	
 	, task_id	uuid		
