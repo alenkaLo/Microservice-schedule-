32879d70b2357426d1841134dfd45f4d44431c14
h1nekora <wonstorm0511>
2025-04-26 13:18:10 +0300

    ref

---
+++ TimeTable/Models/Entity/LessonsToAdd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TimeTable.Models.Entity
+{
+    public class LessonsToAdd
+    {
+        public List<Lesson> Lessons { get; set; }
+        public Lesson  UniqueLesson { get; set; }
+        public LessonsToAdd(List<Lesson> lessons, Lesson uniqueLesson)
+        {
+            Lessons = lessons;
+            UniqueLesson = uniqueLesson;
+        }
+        public void Add(DateOnly time)
+        {
+            UniqueLesson.Date = time;
+            UniqueLesson.Id = Guid.NewGuid();
+            Lessons.Add(UniqueLesson.Clone());
+        }
+    }
+}
